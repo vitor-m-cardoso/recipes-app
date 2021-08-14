d67c76b41cd50416afbf1b274fe324b2eeadb7a4
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-14 01:47:19 -0300

    altera rota do browserrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Routes from './Routes';
 import store from './redux/store';
@@ -7,11 +6,9 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter basename="/recipes-app">
-      <Provider store={ store }>
-        <Routes />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={ store }>
+      <Routes />
+    </Provider>
   );
 }
 
