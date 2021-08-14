fbb761bb0a8dbe875a6a8d381f3fa9f1f93b89d2
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-14 02:06:05 -0300

    remove basename

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <HashRouter basename="/recipes-app">
+  <HashRouter>
     <App />
   </HashRouter>,
   document.getElementById('root'),
