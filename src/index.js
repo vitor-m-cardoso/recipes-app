88ef3f7e57efc7083386229cd2b4efdf86112db4
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-14 02:04:18 -0300

    adiciona hashrouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter basename="/recipes-app">
+  <HashRouter basename="/recipes-app">
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root'),
 );
