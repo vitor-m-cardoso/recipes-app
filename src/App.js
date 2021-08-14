e9527d0f8ee242fccbef9093357751ec153899a8
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-14 01:30:47 -0300

    altera pastas e caminho do app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/recipes-app">
       <Provider store={ store }>
         <Routes />
       </Provider>
